0dfc58bfb776368b1ee76cd50bef2b5a63e93064
John Lemuel G. Hapa <jl.g.hapa@gmail.com>
2025-02-27 00:41:23 +0800

    Change firebase and supabase config for deployment

--- src/store/firebase-config.js
+++ src/store/firebase-config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPsJlWeoIoz_Klbx8_uHPeWCscVqMT96o",
-  authDomain: "nosax-web.firebaseapp.com",
-  projectId: "nosax-web",
-  storageBucket: "nosax-web.firebasestorage.app",
-  messagingSenderId: "68245518546",
-  appId: "1:68245518546:web:8c0d1fe0f10cb1a82b950b",
+  apiKey: "AIzaSyBejkrrtL13tUHmzsCB15QJ8CbSYXmQBRs",
+  authDomain: "nosax-project.firebaseapp.com",
+  projectId: "nosax-project",
+  storageBucket: "nosax-project.firebasestorage.app",
+  messagingSenderId: "458814073424",
+  appId: "1:458814073424:web:6884230d0b75744c22ff55",
 };
 
 // Initialize Firebase
--- src/store/superbaseClient.js
+++ src/store/superbaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://urkwhebgqkxuvntqdqyh.supabase.co";
+const SUPABASE_URL = "https://qjrqdgzbcpnisxkpflzt.supabase.co";
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVya3doZWJncWt4dXZudHFkcXloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1NDAwOTcsImV4cCI6MjA1NjExNjA5N30.Hf4mDKrucjCZbKz-ZOezkDICT15hezNBZY8ZN3aVHEA";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqcnFkZ3piY3BuaXN4a3BmbHp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1NzA3NjYsImV4cCI6MjA1NjE0Njc2Nn0.lfWuqt498uUY3SR-AOphoCuHSbTpXlTGHr7_xExaeqo";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
